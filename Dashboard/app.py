a84229479265f3cf311fa12021f2d5635e3dbbae
Joshitha12734 <joshitha12102005@gmail.com>
2026-04-07 00:17:12 +0530

    Update app.py

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -43,7 +43,7 @@ def login():
     password = st.text_input("Password", type="password")
 
     if st.button("Login"):
-        if username == "admin" and password == "crq123":
+        if username == "admin" and password == "admin123":
             st.session_state["logged_in"] = True
         else:
             st.error("Invalid credentials")
